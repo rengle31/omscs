41c5ba49ee6b59a97b7181adfc062972c9192270
rengle31 <rengle31@gatech.edu>
2026-01-12 01:42:11 +0000

    date fixing

--- omscs.py
+++ omscs.py
@@ -2,13 +2,15 @@
 import pickle   
 import pandas as pd
 import re
-from datetime import date
+import datetime
 import os.path
 import numpy as np
 import streamlit as st
 
-today = date.today()
-file_path = f"dataframe_{today}.pkl"
+
+today = datetime.datetime.now().replace(microsecond=0)
+file_path = f"dataframe.pkl"
+
 if not os.path.isfile(file_path):
     print(f"The file {file_path} DNE - no pickle - ERROR.")
     quit()
@@ -17,8 +19,12 @@ else:
         pickle_df = pickle.load(f)
     print(f"loaded {file_path}")
 
-    dataframe_date = file_path.removeprefix("dataframe_").removesuffix(".pkl")
-    
+    m_timestamp = os.path.getmtime(file_path)
+    dataframe_date = datetime.datetime.fromtimestamp(m_timestamp).replace(microsecond=0)
+
+    data_age = today-dataframe_date
+
+    print(f"data age: {data_age}")
 
 df = pickle_df[['Title', 'Course Number', 'Section', 'CRN', 'Status']]
 df = df.loc[df['Section'].str.startswith('O') & df['Section'].str[1].str.isdigit()] #== 'O01']
@@ -37,7 +43,6 @@ df = df.drop(['Status'], axis=1)
 df.sort_values(by='Seats Left', ascending=False, inplace=True)
 df.drop_duplicates(ignore_index=True, inplace=True)
 
-
 conditions = [
     (df['% Fill Rate'] >= 100),
     (df['% Fill Rate'] >= 75) & (df['% Fill Rate'] < 100),
@@ -46,36 +51,16 @@ conditions = [
 choices = ["🔴", "🟠", "🟢"]
 df['Status'] = np.select(conditions, choices, default='')
 
-
-# col = df.pop('Status')
-# df.insert(0, 'Status', col)
-
-# keywords = ['law', 'marketing', 'os', 'process', 'language', 'deep', 'artificial', 'graphics', 'GPU', 'game', 'video', 'learning']
-
-# pattern = '|'.join(keywords)
-# results = df[df['Title'].str.contains(pattern, case=False, na=False)].sort_values(by='Seats Left', ascending=False)
-# print(dataframe_date)
-# results
-
-# import aka
-
-# courses = ['RL', 'RAIT']
-
-# keywords = [aka.aka[k] for k in courses if k in courses]
-# pattern = '|'.join(keywords)
-# results = df[df['Title'].str.contains(pattern, case=False, na=False)].sort_values(by='Seats Left', ascending=False)
-# print(dataframe_date)
-# results
-
-
+col = df.pop('Status')
+df.insert(0, 'Status', col)
 
 st.set_page_config(layout="wide")
-
 left, middle, right = st.columns([1, 8, 1])
 
 with middle:
-    st.markdown(f"### OMSCS Course Occupancy - {dataframe_date}")
-    search_term = st.text_input("Enter search term (keyword or exact course number):")
+    st.markdown(f"## OMSCS Course Occupancy")
+    st.markdown(f"Data Age: {data_age}, \tData Timestamp: {dataframe_date}utc")
+    search_term = st.text_input("Enter search term (keyword or exact course number, acronyms not yet supported):")
     if search_term:
         if search_term.isdigit(): df = df[df['Course Number'] == (search_term)]
         else: df = df[df['Title'].str.contains(search_term, case=False, na=False)]
@@ -87,7 +72,6 @@ with middle:
             max_value=100,
             color="blue"
         )
-        
     })
 
 
